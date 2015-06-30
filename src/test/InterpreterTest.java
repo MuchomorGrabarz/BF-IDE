e5f11c0d530e47dda886f73c18c363ad877e1526
Szymon Śliwa <knight.erraunt@gmail.com>
2015-06-30 13:09:00 +0200

    Added more interpreter tests

--- src/test/InterpreterTest.java
+++ src/test/InterpreterTest.java
@@ -53,4 +53,46 @@ public class InterpreterTest {
 
         verify(stream).setText("ba");
     }
+
+    @Test
+    public void testLeftBoundCheck() throws Exception {
+        IO stream = mock(FXIO.class);
+
+        when(stream.getText()).thenReturn("");
+
+        List<BFNode> mockedCode = mock(ArrayList.class);
+
+        when(mockedCode.get(0)).thenReturn(new BFNode('<'));
+
+        when(mockedCode.size()).thenReturn(new Integer(1));
+
+        Interpreter testedObj = new Interpreter(stream);
+        testedObj.run(mockedCode);
+
+        verify(stream).alert("Program moved to negative tape indexes");
+    }
+
+    @Test
+    public void testRightBoundCheck() throws Exception {
+        //to be done, need to know how to get tape length first
+        return;
+    }
+
+    @Test
+    public void testInsufficientInput() throws Exception {
+        IO stream = mock(FXIO.class);
+
+        when(stream.getText()).thenReturn("");
+
+        List<BFNode> mockedCode = mock(ArrayList.class);
+
+        when(mockedCode.get(0)).thenReturn(new BFNode(','));
+
+        when(mockedCode.size()).thenReturn(new Integer(1));
+
+        Interpreter testedObj = new Interpreter(stream);
+        testedObj.run(mockedCode);
+
+        verify(stream).alert("Insufficient input given");
+    }
 }
